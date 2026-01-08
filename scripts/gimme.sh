c69fcf027f6442f022bf306f2bb8bad463050821
Jerrod <jerrod@kernelle.net>
2026-01-08 10:54:49 -0700

    wrapper actually added this time

---
+++ scripts/gimme.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function gimme() {
+    local output
+    output="$(command gimme "$@")"
+    local exit_code="$?"
+    
+    if [[ "$output" == cd://* ]]; then
+        cd "${output#cd://}" || return 1
+    else
+        return $exit_code
+    fi
+}
